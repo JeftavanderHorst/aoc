550ae9727dc2b607a2ac5a98c18907d9a2046f59
Jefta <info@jefta.eu>
2021-11-30 16:47:56 +0100

    2019 Day 1 part 2

--- 2019/day1/src/main/kotlin/Main.kt
+++ 2019/day1/src/main/kotlin/Main.kt
@@ -1,11 +1,21 @@
 import java.io.File
 import kotlin.math.floor
 
+fun computeFuel(mass: Double): Double {
+    var result = floor(mass / 3) - 2
+
+    if (result > 6) {
+        result += computeFuel(result)
+    }
+
+    return result
+}
+
 fun main() {
     println(
         File("input.txt")
             .readLines()
             .map { it.toDouble() }
-            .sumOf { mass -> floor(mass / 3) - 2 }
+            .sumOf { mass -> computeFuel(mass) }
     )
 }
