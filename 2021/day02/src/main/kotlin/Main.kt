de148543276fc148573758f4ac872eeb1a66482e
Jefta <info@jefta.eu>
2021-12-02 13:12:09 +0100

    2021 Day 2 part 2

--- 2021/day02/src/main/kotlin/Main.kt
+++ 2021/day02/src/main/kotlin/Main.kt
@@ -1,20 +1,26 @@
 import java.io.File
 
-data class Pos(val x: Int, val y: Int)
+data class Pos(var x: Long, var y: Long)
 
 fun main() {
-    val pos = File("input.txt")
+    val commands = File("input.txt")
         .readLines()
         .map { it.split(' ') }
-        .fold(Pos(0, 0)) { acc, command ->
-            val delta = command[1].toInt()
-            when (command[0]) {
-                "forward" -> Pos(acc.x + delta, acc.y)
-                "up" -> Pos(acc.x, acc.y - delta)
-                "down" -> Pos(acc.x, acc.y + delta)
-                else -> throw Exception("Invalid command")
+
+    val pos = Pos(0, 0)
+    var aim: Long = 0
+
+    for (command in commands) {
+        val delta = command[1].toLong()
+        when (command[0]) {
+            "up" -> aim -= delta
+            "down" -> aim += delta
+            "forward" -> {
+                pos.x += delta
+                pos.y += aim * delta
             }
         }
+    }
 
     println(pos.x * pos.y)
 }
