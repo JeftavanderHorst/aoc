53a7fd8292ce7f52bd44a3c52e4788557bee8db1
Jefta <info@jefta.eu>
2021-12-06 14:39:47 +0100

    2021 Day 6 part 1

---
+++ 2021/day06/main.cpp
@@ -0,0 +1,51 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+void PrintFishes(uint16_t day, uint64_t *fishes) {
+  std::cout << "After " << day << " days: ";
+
+  uint64_t number_of_fishes = 0;
+  for (uint8_t i = 0; i < 10; i++) {
+    std::cout << std::to_string(fishes[i]) << ", ";
+    number_of_fishes += fishes[i];
+  }
+
+  std::cout << " (" << number_of_fishes << " fishes)" << std::endl;
+}
+
+int main() {
+  std::ifstream ifs("input.txt");
+  std::string content((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+  std::stringstream input(content);
+
+  // This pointer is incremented by one each tick, so that it always points to the current day
+  // I.e. fishes[0] is always the day where fishes reproduce
+  auto fishes = new uint64_t[100] {0};
+
+  std::string substr;
+  while (std::getline(input, substr, ',')) {
+    fishes[std::stoi(substr)]++;
+  }
+
+  PrintFishes(0, fishes);
+
+  for (uint16_t day = 1; day <= 80; day++) {
+    fishes[7] += fishes[0];
+    fishes[9] += fishes[0];
+
+    // Move pointer
+    fishes++;
+
+    PrintFishes(day, fishes);
+  }
+
+  uint64_t number_of_fishes = 0;
+  for (uint8_t i = 0; i < 10; i++) {
+    number_of_fishes += fishes[i];
+  }
+  std::cout << "There are " << number_of_fishes << " fishes" << std::endl;
+
+  return 0;
+}
