8e5ba56144d1fe1375ac88519b753dadb56dda07
Jefta <info@jefta.eu>
2021-12-09 13:22:13 +0100

    2021 Day 9 part 2

--- 2021/day09/src/main/kotlin/Main.kt
+++ 2021/day09/src/main/kotlin/Main.kt
@@ -5,11 +5,28 @@ data class Pos(val x: Int, val y: Int)
 fun main() {
     val lines = File("input.txt")
         .readLines()
-        .map { it.toCharArray().map { it.digitToInt() } }
+        .map { it.toCharArray().map { height -> height.digitToInt() } }
 
     fun inBounds(pos: Pos): Boolean = lines.indices.contains(pos.y) && lines[0].indices.contains(pos.x)
 
-    val lows = mutableListOf<Int>()
+    val assignedToBasin = mutableSetOf<Pos>()
+
+    fun measureBasin(height: Int, pos: Pos): Int {
+        if (lines[pos.y][pos.x] >= 9 || assignedToBasin.contains(pos)) return 0
+
+        assignedToBasin.add(pos)
+
+        return 1 + listOf(
+            Pos(pos.x - 1, pos.y),
+            Pos(pos.x + 1, pos.y),
+            Pos(pos.x, pos.y - 1),
+            Pos(pos.x, pos.y + 1),
+        )
+            .filter { inBounds(it) }
+            .sumOf { measureBasin(height + 1, it) }
+    }
+
+    val sizes = mutableListOf<Int>()
     for (y in lines.indices) {
         for (x in lines[0].indices) {
             val isLowPoint = listOf(
@@ -22,10 +39,12 @@ fun main() {
                 .all { lines[it.y][it.x] > lines[y][x] }
 
             if (isLowPoint) {
-                lows.add(lines[y][x] + 1)
+                sizes.add(measureBasin(lines[y][x], Pos(x, y)))
+                println("Basin $x x $y has size ${sizes.last()}")
             }
         }
     }
 
-    println(lows.sum())
+    sizes.sortDescending()
+    println("${sizes[0]} * ${sizes[1]} * ${sizes[2]} = ${sizes[0] * sizes[1] * sizes[2]}")
 }
