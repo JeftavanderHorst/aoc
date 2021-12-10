305edc0c13b71e24047e50c4ed50b01ac2ecead6
Jefta <info@jefta.eu>
2021-12-10 12:07:04 +0100

    2021 Day 10 part 2

--- 2021/day10/src/main/kotlin/Main.kt
+++ 2021/day10/src/main/kotlin/Main.kt
@@ -6,29 +6,47 @@ class Parser(private val string: String) {
     private var index: Int = 0
     private val stack = ArrayDeque(emptyList<Char>())
 
-    fun getScore(): Int {
+    fun getCompletion(): String? {
         while (index < string.length) {
             when (string[index++]) {
                 '(' -> stack.addLast(')')
                 '[' -> stack.addLast(']')
                 '{' -> stack.addLast('}')
                 '<' -> stack.addLast('>')
-                ')' -> if (stack.last() == ')') stack.removeLast() else return 3
-                ']' -> if (stack.last() == ']') stack.removeLast() else return 57
-                '}' -> if (stack.last() == '}') stack.removeLast() else return 1197
-                '>' -> if (stack.last() == '>') stack.removeLast() else return 25137
+                ')' -> if (stack.last() == ')') stack.removeLast() else return null
+                ']' -> if (stack.last() == ']') stack.removeLast() else return null
+                '}' -> if (stack.last() == '}') stack.removeLast() else return null
+                '>' -> if (stack.last() == '>') stack.removeLast() else return null
                 else -> throw Exception()
             }
         }
 
-        return 0
+        return stack.joinToString("").reversed()
     }
 }
 
+fun computeScore(completion: String): Long {
+    var score: Long = 0
+    for (c in completion.toCharArray()) {
+        score *= 5
+        score += when (c) {
+            ')' -> 1
+            ']' -> 2
+            '}' -> 3
+            '>' -> 4
+            else -> throw Exception()
+        }
+    }
+
+    return score
+}
+
 fun main() {
-    println(
-        File("input.txt")
-            .readLines()
-            .sumOf { Parser(it).getScore() }
-    )
+    val scores = File("input.txt")
+        .readLines()
+        .mapNotNull { Parser(it).getCompletion() }
+        .map { computeScore(it) }
+        .sorted()
+
+    println(scores[scores.size / 2])
 }
